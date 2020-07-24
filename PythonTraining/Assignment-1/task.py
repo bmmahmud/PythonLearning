745bdfa5c42f27a09b316b7b301d39690030e46d
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2020-07-24 08:48:13 +0600

    covid 19

---
+++ PythonTraining/Assignment-1/task.py
@@ -0,0 +1,14 @@
+#
+# Python Training Day 1: Assignment 1
+#
+name = 'B.M. ASHIK MAHMUD'
+empID = 96194899
+designation = 'Jr System Analyst'
+address = 'Gulshan-2'
+print('Name: '+name+'\n'+'Employee ID: '+
+      str(empID)+'\n'+
+      'Designamtion: '+designation+'\n'+
+      'Address: '+address+'\n'
+      )
+print(type(name))
+print(type(empID))
