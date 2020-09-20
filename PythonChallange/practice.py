d73a87e5a9e8133d30200c07684edfd0e1b6ea1c
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2020-09-20 21:35:46 +0600

    python learning

---
+++ PythonChallange/practice.py
@@ -0,0 +1,25 @@
+# Python Learning Challange 
+# Day - 1 20-Sep-2020
+
+# num = 25
+# name = 'Ashik'
+
+# print("My name is {} and I am {} years old.".format(name,num))
+# print("My name is {one} and I am {two} years old.".format(one=name,two=num))
+
+#String
+# s ='hello'
+# print(s[1])
+x = 'abcdefgh'
+# print(x[3:])
+# print(x[:3])
+# print(x[1:3]) # srat with x = 1 but x<3
+
+# # List
+# myList = ['a','b','c','d',[1,2,['target']]]
+# # myList.append('e') # add in the end
+# # print(myList[2:4])
+# myList[3] = 'New'
+# # print(myList[4][1])
+# print(myList[4][2][2])
+
