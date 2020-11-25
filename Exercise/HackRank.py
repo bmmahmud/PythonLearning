e132b4e5006c15ae4ea93f27d6646a342d60fad8
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2020-11-25 13:35:02 +0600

    aa

--- Exercise/HackRank.py
+++ Exercise/HackRank.py
@@ -1,7 +1,27 @@
-# print('hi')
-lst = []
-n = int(input())
-list =[2,5,1,3,4]
-for i in range(0,n):
-    arr = map(int, input().split()
-print (sorted(set(arr))[-2])    
+# Animal
+lists = ["vertebrado","ave","onivoro"]
+# for x in range(3):
+#     aa = input()
+#     lists.append(aa)  
+if lists[0] == 'vertebrado':
+    if lists[1] == 'mamifero':
+        if lists[2] == 'onivoro':
+            print('homem')
+        elif lists[2] == 'herbivoro':
+            print('vaca')
+    elif lists[1] == 'ave':
+        if lists[2] == 'carnivoro':
+            print('aguia')
+        if lists[2] == 'onivoro':
+            print('pomba')
+elif lists[0] == 'invertebrado':
+    if lists[1] == 'inseto':
+        if lists[2] == 'hematofago':
+            print('pulga')
+        elif lists[2] == 'herbivoro':
+            print('lagarta')
+    elif lists[1] == 'anelideo':
+        if lists[2] == 'hematofago':
+            print('sanguessuga')
+        elif lists[2] == 'onivoro':
+            print('minhoca')            
